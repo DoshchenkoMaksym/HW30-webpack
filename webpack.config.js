f300b1a43d063ab0c2c572ecdfdc34922bddc4b7
Maksym Doshchenko <doschenko54@gmail.com>
2021-10-20 10:52:03 +0300

    some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const path = require('path');
 module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js",
+      },
     plugins: [
         new HtmlWebpackPlugin(),
         new CleanWebpackPlugin(),
     ],
+    devServer: {
+        port: 8000,
+    },
     mode: 'production',
 }
